b1180e308b2f72508f44500cb119cbe2b8b7e714
dilidili <466311467@qq.com>
2017-02-26 22:01:51 +0800

    包装对象-doing

--- 读书笔记/javascript高级程序设计/text.js
+++ 读书笔记/javascript高级程序设计/text.js
@@ -1,13 +1,4 @@
-function sum(num1, num2) {
-    return num1 + num2;
-}
-
-function callSum1(num1,num2) {
-    return sum.apply(this,arguments);
-}
-
-function callSum2(num1, num2) {
-    return sum.apply(this,[num1, num2]);
-}
-callSum1(1,2);
-callSum2(1,2);
+var numObj = new Number(10);
+var num = 10;
+typeof numObj;
+typeof num;
